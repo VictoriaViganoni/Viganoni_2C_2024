83420fbed26a1160411cc067317e025b15e37134
maria victoria viganoni <maria.viganoni@ingenieria.uner.edu.ar>
2024-09-20 14:46:38 -0300

    Finalice los ejercicios 1 y 2 del proyecto 2

--- firmware/projects/guia2_ej1/main/guia2_ej1.c
+++ firmware/projects/guia2_ej1/main/guia2_ej1.c
@@ -1,4 +1,4 @@
-/*! @mainpage Template
+/*! @mainpage Proyecto 2: ejercicio 1
  *
  * @section genDesc General Description
  *
@@ -17,23 +17,169 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 06/09/2024 | Document creation		                         |
+ * 
+ * @section Diseñar el firmware modelando con un diagrama de flujo de manera que cumpla con las siguientes funcionalidades:
+ *
+ *Mostrar distancia medida utilizando los leds de la siguiente manera:
+ *
+ *Si la distancia es menor a 10 cm, apagar todos los LEDs.
+ *Si la distancia está entre 10 y 20 cm, encender el LED_1.
+ *Si la distancia está entre 20 y 30 cm, encender el LED_2 y LED_1.
+ *Si la distancia es mayor a 30 cm, encender el LED_3, LED_2 y LED_1.
+ *
+ *Mostrar el valor de distancia en cm utilizando el display LCD.
+ *Usar TEC1 para activar y detener la medición.
+ *Usar TEC2 para mantener el resultado (“HOLD”).
+ *Refresco de medición: 1 s
+ *
+ *Se deberá conectar a la EDU-ESP un sensor de ultrasonido HC-SR04 y una pantalla LCD y utilizando los drivers provistos por la cátedra 
+ *implementar la aplicación correspondiente. Se debe subir al repositorio el código. Se debe incluir en la documentación, realizada con doxygen, el diagrama de flujo. 
  *
  * @author maria victoria viganoni (maria.viganoni@ingeniera.uner.edu.ar)
- *
  */
-
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "gpio_mcu.h"
+#include "lcditse0803.h"
+#include "switch.h"
+#include "led.h"
+#include "hc_sr04.h"
 /*==================[macros and definitions]=================================*/
+/** @def REFRESH_TECLAS
+ * @brief Define el tiempo en milisegundos para el retardo en la tarea de gestión de teclas.
+ */
+#define REFRESH_TECLAS 50
+
+/** @def REFRESH_MEDICION
+ * @brief Define el tiempo en milisegundos para el retardo en la tarea de medición con el sensor ultrasónico.
+ */
+#define REFRESH_MEDICION 1000
+
+/** @def REFRESH_DISPLAY
+ * @brief Define el tiempo en milisegundos para el retardo en la actualización de la pantalla LCD.
+ */
+#define REFRESH_DISPLAY 100
 
 /*==================[internal data definition]===============================*/
+/** 
+ * @def distancia
+ * @brief Variable global de tipo entero sin signo que almacena la distancia medida por el sensor ultrasónico en centímetros.
+ */
+uint16_t distancia = 0;
+
+/** 
+ * @def hold
+ * @brief Variable global booleana que indica si se debe mantener el último valor de distancia medido en el display.
+ */
+bool hold = false;
+
+/** 
+ * @def on
+ * @brief Variable global booleana que indica si el sistema de medición está activo o inactivo.
+ */
+bool on = true;
 
 /*==================[internal functions declaration]=========================*/
+/**
+ * @brief Esta tarea realiza las mediciones de distancia usando el sensor ultrasónico.
+ * Actualiza la variable "distancia" y realiza un retraso conforme al intervalo de medición especificado.
+ */
+void sensarTask()
+{ 
+    while (true)
+    {
+        if (on)
+        {
+            distancia = HcSr04ReadDistanceInCentimeters();
+        }
+        vTaskDelay(REFRESH_MEDICION / portTICK_PERIOD_MS);
+    }
+}
 
+/**
+ * @brief Esta tarea muestra los datos medidos en la pantalla LCD y controla el encendido de los LEDs.
+ * Dependiendo de la distancia medida, enciende o apaga los LEDs correspondientes. Además, si no está activado el modo "hold", 
+ * actualiza el valor mostrado en el display.
+ */
+void mostrarTask()
+{
+    while (true)
+    {
+        if (on)
+        {
+            if (distancia < 10)
+            {
+                LedsOffAll();
+            }
+            else if ((distancia > 10) & (distancia < 20))
+            {
+                LedOn(LED_1);
+                LedOff(LED_2);
+                LedOff(LED_3);
+            }
+            else if ((distancia > 20) & (distancia < 30))
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOff(LED_3);
+            }
+            else if (distancia > 30)
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOn(LED_3);
+            }
+
+            if (!hold)
+            {
+                LcdItsE0803Write(distancia);
+            }
+        }
+        else
+        {
+            LcdItsE0803Off();
+            LedsOffAll();
+        }
+        vTaskDelay(REFRESH_DISPLAY / portTICK_PERIOD_MS);
+    }
+}
+/**
+ * @brief Esta tarea gestiona el control de la aplicación utilizando las teclas.
+ * Permite activar o desactivar la medición con TEC1 y mantener el valor de la medición con TEC2.
+ */
+void teclasTask()
+{
+    uint8_t teclas;
+    while (true)
+    {
+        teclas = SwitchesRead();
+        switch (teclas)
+        {
+        case SWITCH_1:
+            on = !on;
+            break;
+        case SWITCH_2:
+            hold = !hold;
+            break;
+        }
+        vTaskDelay(REFRESH_TECLAS / portTICK_PERIOD_MS);
+    }
+}
 /*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+void app_main(void)
+{
+    LedsInit();
+    HcSr04Init(GPIO_3, GPIO_2);
+    LcdItsE0803Init();
+    SwitchesInit();
+
+    xTaskCreate(&sensarTask, "medir", 512, NULL, 5, NULL);
+    xTaskCreate(&teclasTask, "mostrar", 512, NULL, 5, NULL);
+    xTaskCreate(&mostrarTask, "teclas", 512, NULL, 5, NULL);
 }
 /*==================[end of file]============================================*/
---
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -0,0 +1,243 @@
+/*! @mainpage Proyecto 2: ejercicio 2
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 13/09/2024 | Document creation		                         |
+ * 
+ * @sectionCree un nuevo proyecto en el que modifique la actividad del punto 1 de manera de utilizar interrupciones 
+ * para el control de las teclas y el control de tiempos (Timers). 
+ *
+ *
+ * @author maria victoria viganoni (maria.viganoni@ingeniera.uner.edu.ar)
+ *
+ */
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "hc_sr04.h"
+#include "gpio_mcu.h"
+#include "lcditse0803.h"
+#include "switch.h"
+#include "timer_mcu.h"
+/*==================[macros and definitions]=================================*/
+
+/** @def REFRESH_MEDICION
+ * @brief Define el tiempo en milisegundos para el retardo en la tarea de medición con el sensor ultrasónico.
+ */
+#define REFRESH_MEDICION 1000000
+
+/** @def REFRESH_DISPLAY
+ * @brief Define el tiempo en milisegundos para el retardo en la actualización de la pantalla LCD.
+ */
+#define REFRESH_DISPLAY 100000
+
+
+/*==================[internal data definition]===============================*/
+/**
+ * @def Sensar_task_handle 
+ * @brief Manejador de la tarea encargada de sensar.
+ * Este objeto es utilizado para gestionar la tarea que realiza las funciones
+ * de sensado dentro del sistema. Permite la manipulación y notificación de la 
+ * tarea desde otras partes del código, incluyendo la reanudación o suspensión
+ * de su ejecución.
+ */
+TaskHandle_t Sensar_task_handle = NULL;
+
+/**
+ * @def Mostrar_task_handle
+ * @brief Manejador de la tarea encargada de mostrar información.
+ * Este objeto permite gestionar la tarea responsable de mostrar información en
+ * el sistema. Se utiliza para notificar, suspender o reanudar dicha tarea desde
+ * otros contextos de ejecución, como interrupciones o tareas concurrentes.
+ */
+TaskHandle_t Mostrar_task_handle = NULL;
+
+/** 
+ * @def distancia
+ * @brief Variable global de tipo entero sin signo que almacena la distancia medida por el sensor ultrasónico en centímetros.
+ */
+uint16_t distancia = 0;
+
+/** 
+ * @def hold
+ * @brief Variable global booleana que indica si se debe mantener el último valor de distancia medido en el display.
+ */
+bool hold = false;
+
+/** 
+ * @def on
+ * @brief Variable global booleana que indica si el sistema de medición está activo o inactivo.
+ */
+bool on = true;
+
+/*==================[internal functions declaration]=========================*/
+
+/**
+ * @fn FuncTimerSensar(void *param)
+ * @param param parámetro que no se utiliza
+ * @brief Envía una notificación a la tarea encargada de sensar.
+ * Esta función se utiliza para notificar de manera asíncrona a la tarea de sensado, 
+ * desencadenando su ejecución. Se emplea en un contexto de interrupción, utilizando 
+ * la función vTaskNotifyGiveFromISR para evitar bloqueos en la ejecución del sistema.
+ */
+void FuncTimerSensar(void *param)
+{
+    vTaskNotifyGiveFromISR(Sensar_task_handle, pdFALSE); 
+}
+
+/**
+ * @fn void FuncTimerMostrar(void *param)
+ * @param param parámetro que no se utiliza
+ * @brief Envía una notificación a la tarea encargada de mostrar.
+ * Esta función notifica de manera asíncrona a la tarea responsable de mostrar información, 
+ * desencadenando su ejecución. Se ejecuta en un contexto de interrupción, usando la función 
+ * vTaskNotifyGiveFromISR, asegurando así una operación eficiente sin bloqueos del sistema.
+ */
+void FuncTimerMostrar(void *param)
+{
+    vTaskNotifyGiveFromISR(Mostrar_task_handle, pdFALSE); /* Envía una notificación a la tarea asociada al mostrar */
+}
+
+/**
+ * @brief Esta tarea realiza las mediciones de distancia usando el sensor ultrasónico.
+ * Actualiza la variable "distancia" y realiza un retraso conforme al intervalo de medición especificado.
+ */
+void sensarTask()
+{
+    while (true)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); /*la tarea espera en este punto hasta recibir la notificacion*/
+        if (on)
+        {
+            distancia = HcSr04ReadDistanceInCentimeters();
+        }
+    }
+}
+
+/**
+ * @brief Esta tarea muestra los datos medidos en la pantalla LCD y controla el encendido de los LEDs.
+ * Dependiendo de la distancia medida, enciende o apaga los LEDs correspondientes. Además, si no está activado el modo "hold", 
+ * actualiza el valor mostrado en el display.
+ */
+void mostrarTask()
+{
+    while (true)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 
+        if (on)
+        {
+            if (distancia < 10)
+            {
+                LedsOffAll();
+            }
+            else if ((distancia > 10) & (distancia < 20))
+            {
+                LedOn(LED_1);
+                LedOff(LED_2);
+                LedOff(LED_3);
+            }
+
+            else if ((distancia > 20) & (distancia < 30))
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOff(LED_3);
+            }
+
+            else if (distancia > 30)
+            {
+                LedOn(LED_1);
+                LedOn(LED_2);
+                LedOn(LED_3);
+            }
+
+            if (!hold)
+            {
+                LcdItsE0803Write(distancia);
+            }
+        }
+
+        else
+        {
+            LcdItsE0803Off();
+            LedsOffAll();
+        }
+    }
+}
+
+/**
+ * @fn TeclaOn()
+ * @brief Alterna el estado de la bandera booleana `on`.
+ * Esta función invierte el valor de la variable booleana `on`. Si el valor de 
+ * `on` es verdadero, lo cambia a falso, y viceversa. Se utiliza para controlar 
+ * el encendido o activación de un proceso o sistema.
+ */
+void TeclaOn()
+{
+    on = !on;
+}
+
+/**
+ * @fn TeclaHold()
+ * @brief Alterna el estado de la bandera booleana `hold` si la bandera `on` está activa.
+ * Esta función cambia el estado de la variable booleana `hold` solo si la 
+ * bandera `on` está activada (es verdadera). Se utiliza para mantener o pausar 
+ * una acción mientras el sistema está encendido.
+ */
+void TeclaHold()
+{
+    if (on)
+    {
+        hold = !hold;
+    }
+}
+
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+    LedsInit();
+    HcSr04Init(GPIO_3, GPIO_2);
+    LcdItsE0803Init();
+    SwitchesInit();
+
+    timer_config_t timer_medicion = {
+        .timer = TIMER_A,
+        .period = REFRESH_MEDICION,
+        .func_p = FuncTimerSensar,
+        .param_p = NULL};
+    TimerInit(&timer_medicion);
+
+    timer_config_t timer_mostrar = {
+        .timer = TIMER_B,
+        .period = REFRESH_DISPLAY,
+        .func_p = FuncTimerMostrar,
+        .param_p = NULL};
+    TimerInit(&timer_mostrar);
+
+    SwitchActivInt(SWITCH_1, &TeclaOn, NULL);
+    SwitchActivInt(SWITCH_2, &TeclaHold, NULL);
+    xTaskCreate(&sensarTask, "sensar", 512, NULL, 5, &Sensar_task_handle);
+    xTaskCreate(&mostrarTask, "mostrar", 512, NULL, 5, &Mostrar_task_handle);
+    TimerStart(timer_medicion.timer);
+    TimerStart(timer_mostrar.timer);
+}
+/*==================[end of file]============================================*/
